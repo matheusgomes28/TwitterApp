a2f3c634573d572278169eb1a304f9af2c5f29b1
Matheus Gomes <mdeaguiargomes2@sheffield.ac.uk>
2015-03-10 18:57:33 +0000

    Added simple settings page. User will then be able to change his/her details.

--- control.rb
+++ control.rb
@@ -163,12 +163,39 @@ get '/campaign' do
 end
 
 get '/show_campaigns' do
+
   #simple select
   query = 'SELECT name, desc, keyword FROM campaigns'
 
   # Send user and campaign results to show_campaigns page
   @camps = @db.execute query
   erb :show_campaigns
+end
+
+get '/settings' do
+
+  # Gettings results from linked tables
+  user_details = @db.execute(%{
+                               SELECT users.password, users.email, users.twitter, settings.consumer_key,
+                               settings.consumer_secret, settings.access_token, settings.access_token_secret
+                               FROM users, settings
+                               WHERE users.username = ? AND users.username = settings.username;
+                              },
+                              session[:username])
+
+  # Sending results to the page
+  puts user_details.length
+
+  @username = session[:username]
+  @password = '*'*user_details[0][0].length
+  @email = user_details[0][1]
+  @twitter = user_details[0][2]
+  @c_key = user_details[0][3]
+  @c_secret = user_details[0][4]
+  @access_token = user_details[0][5]
+  @access_token_s = user_details[0][6]
+
+  erb :settings
 end
 
 
