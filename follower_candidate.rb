a8deb5022cadb3aeee0814307e38dd81df2bc58f
Matheus <mdeaguiargomes1@sheffield.ac.uk>
2015-04-30 22:54:32 +0100

    Created class for a possible user that will be followed by the system automatically. FollowerCandidate holds methods and fields needed for calculating a personal score (that is used to add/remove users from following whitelist).

---
+++ follower_candidate.rb
@@ -0,0 +1,70 @@
+# Class made for calculating
+# a user score to people that
+# are involved with a campign.
+# WRITTEN BY: Matt Gomeszzzzzzzzzz xx
+
+require 'twitter' #Needed for the user and tweet objects
+
+class FollowerCandidate
+
+  # Criteria value for retweets and favorites
+  FAVORITE_SCORE = 1
+  RETWEET_SCORE = 3
+
+  def initialize(rest_client, twitter_user, campaign_tweet)
+    @client = rest_client
+    @candidate = twitter_user
+    @camp_tweet  = campaign_tweet
+  end
+
+  # This methods simply checks if
+  # the user has a profile picture
+  # to be used in the user score
+  def has_picture?
+
+    # Simple if else check
+    if @candidate.profile_image_uri? then
+      return true
+
+    else
+      return false
+    end
+  end
+
+
+  # This method checks the activity
+  # of a twitter user and returns
+  # the time (in days) that he/she
+  # was last active.
+  def last_active
+
+    # Getting last tweet off user timeline
+    last_tweet = @client.user_timeline(@candidate).take(1)[0]
+
+    # Using Time class to get the difference in seconds then days
+    days_inactive = (Time.now - last_tweet.created_at) / 86400
+
+    puts @candidate.screen_name
+    puts days_inactive.floor
+
+    return days_inactive.floor #  Return # of days inactive
+
+  end
+
+
+  # This method will calculate the
+  # user score related to the a
+  # certain campaign that the user retweeted
+  def campaign_score
+
+    final_score = 0 # Starts with no score
+
+    # Calculate the points gained from campaign
+    final_score += @camp_tweet.favorite_count*FAVORITE_SCORE
+    final_score += @camp_tweet.retweet_count*RETWEET_SCORE
+
+    return final_score
+  end
+
+
+end
